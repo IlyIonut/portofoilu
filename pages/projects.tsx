da8c761d82afa3170a5d902fb016062fce25e114
IlyIonut <iliesionut3@gmail.com>
2023-02-23 13:55:34 +0200

    SEO1

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -6,6 +6,7 @@ import { projects as projectsData } from "../data"
 import { Category } from "../type"
 import {motion} from 'framer-motion'
 import { fadeInUp, routeAnimation, stagger } from "../animations"
+import Head from "next/head"
 
 const Projects = () => {
 
@@ -26,6 +27,11 @@ const Projects = () => {
 
     return(
         <motion.div variants={routeAnimation} initial="initial" animate="animate" exit="exit" className="px-5 py-2 overflow-y-scroll" style ={{height:"65vh"}}>
+           <head>
+                <title>
+                    Web Developer | Project | Ionut
+                </title>
+            </head>
            <ProjectsNavbar handlerFilterCategory = {handlerFilterCategory} active={active} />
             <motion.div variants={stagger} className="relative grid grid-cols-12 gap-4 my-3">
             {
