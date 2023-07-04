6558c9a48751733a06cd923b1f8d31838909c401
IlyIonut <iliesionut3@gmail.com>
2023-07-04 22:47:55 +0300

    schimbari

--- data.ts
+++ data.ts
@@ -84,6 +84,17 @@ export const projects:IProject[] = [
     id:1,
     name: "Smart Brain",
     description:
+      "This is an application created for the purpose of undergraduate work. The purpose of the application is to offer users a place where they can find other people depending on the skills they are looking for. The application uses Google Firebase as a database.",
+    image_path: "/images/SkillConnect.PNG",
+    deployed_url: "https://taupe-creponne-a6b5d7.netlify.app/",
+    github_url: "https://github.com/IlyIonut/licenta",
+    category: ["React"],
+    key_techs: ["React"],
+  },
+  { 
+    id:2,
+    name: "Smart Brain",
+    description:
       "In this application we have implemented a face-recognition Api together with a server and a database called on AWS. To work, download the repository and run npm start.",
     image_path: "/images/smartbrain.png",
     deployed_url: "https://ilyionut.github.io/facerecognition/",
@@ -92,7 +103,7 @@ export const projects:IProject[] = [
     key_techs: ["React"],
   },
   {
-    id:2,
+    id:3,
     name: "RoboFriends",
     image_path: "/images/RoboFriends.png",
     deployed_url: "https://ilyionut.github.io/robofriends/",
@@ -103,7 +114,7 @@ export const projects:IProject[] = [
     key_techs: ["React"],
   },
   {
-    id:3,
+    id:4,
     name: "ZeroWaste",
     image_path: "/images/A3_Prezentare.png",
     deployed_url: "/",
@@ -114,7 +125,7 @@ export const projects:IProject[] = [
     key_techs: ["Photoshop"],
   },
   {
-    id:4,
+    id:5,
     name: "The Complete Web Developer in 2023: Zero to Mastery",
     image_path: "/images/Certificat_Curs_WebDevelopment.jpg",
     deployed_url: "https://www.udemy.com/course/the-complete-web-developer-zero-to-mastery/",
@@ -125,7 +136,7 @@ export const projects:IProject[] = [
     key_techs: ["Courses"],
   },
   {
-    id:5,
+    id:6,
     name: "Promotional materials for a party",
     image_path: "/images/Prezentare_party_post.png",
     deployed_url: "/",
@@ -136,7 +147,7 @@ export const projects:IProject[] = [
     key_techs: ["Photoshop"],
   },
   {
-    id:6,
+    id:7,
     name: "Promotional materials for SAS UTCN events",
     image_path: "/images/Demoday.png",
     deployed_url: "/",
